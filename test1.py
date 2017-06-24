cf7d880ecbefb656d25891cbe12ad1199c54029a
Gavia <457821035@qq.com>
2017-06-24 09:21:46 +0800

    with new feature

--- test1.py
+++ test1.py
@@ -31,3 +31,34 @@ w = "This is the left side of ..."
 e = "a string with a right side."
 
 print w + e
+print "======"
+print w,e
+print "======"
+print w
+print e
+print "----------------------------"
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print"""
+With the three double-quotes
+we will be able to type as much as we like.
+Even 4 lines if we want
+"""
+
+print "How old are you?",      ##不输出换行符
+#age = raw_input()
+print "How tall are you"
+#height = raw_input("How tall are yo")
+#print " %s years old, %s tall" % (age , height)
+
+from sys import argv
+
+script_name, first_para, second_para, third_para = argv
+print "the script name is ", script_name , "with first" , first_para ,"and second" , second_para
+print "this is"
+
